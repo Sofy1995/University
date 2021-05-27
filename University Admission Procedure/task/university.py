bea977294f8e96515c032764a17053d2c04041b0
Sofy1995 <convallaria666@gmail.com>
2021-05-27 16:27:38 +0300

    stage 4 completed

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -23,20 +23,54 @@
 # for num in range(m):
 #     print(sorted_applicants[num][0])
 
+def distribute_for_deps(priority):
+    for person in applicants:
+        if len(department[person[priority + 2]]) < max_of_apps:
+            department[person[priority + 2]].append([person[0], person[1], person[2]])
+
+
+def sort_deps():
+    for key in department.keys():
+        sorted_applicants = sorted(department[key], key=lambda x: (-x[2], x[0]))
+        department[key] = sorted_applicants
+
+
+def del_accepted_app():
+    accepted = list()
+    non_accepted = list()
+    for key in department.keys():
+        accepted += department[key][:min(max_of_apps, len(department[key]))]
+    for applicant in applicants:
+        if applicant[:3] not in accepted:
+            non_accepted.append(applicant)
+    return non_accepted
+
+
+def print_accepted():
+    for key in department.keys():
+        print(key)
+        for i in range(min(max_of_apps, len(department[key]))):
+            print(*department[key][i])
+        print("")
+
+
 max_of_apps = int(input())
-department = {'Mathematics': list(), 'Physics': list(), 'Biotech': list(), 'Chemistry': list(), 'Engineering': list()}
+department = {'Biotech': list(), 'Chemistry': list(), 'Engineering': list(), 'Mathematics': list(), 'Physics': list()}
 applicants = list()
+
+
 with open("applicant_list.txt", "r") as apps:
     for line in apps:
-        # app_items = line.split()
-        applicants.append(line.split())
-
-for person in applicants:
-    department[person[3]].append([person[0], person[1], float(person[2])])
+        app_items = line.split()
+        app_items[2] = float(app_items[2])
+        applicants.append(app_items)
 
 
-for key in department.keys():
-    sorted_applicants = sorted(department[key], key = lambda x: (-x[2], x[0]))
-    department[key] = sorted_applicants
 
+for prioritet in [1,2,3]:
+    applicants = sorted(applicants, key=lambda x: (-x[2], x[0]))
+    distribute_for_deps(prioritet)
+    applicants = del_accepted_app()
 
+sort_deps()
+print_accepted()
